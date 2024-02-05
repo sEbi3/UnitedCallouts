b63522053453ac04fc4cc4d65836df72d62748be
AstroBurgers <astroburgers1@gmail.com>
2024-02-05 12:04:35 -0600

    Refactored TrafficStopBackupRequired.cs

--- Callouts/TrafficStopBackupRequired.cs
+++ Callouts/TrafficStopBackupRequired.cs
@@ -1,55 +1,63 @@
-﻿namespace UnitedCallouts.Callouts;
+﻿using System.Linq;
+
+namespace UnitedCallouts.Callouts;
 
 [CalloutInfo("[UC] Traffic Stop Backup Required", CalloutProbability.Medium)]
 public class TrafficStopBackupRequired : Callout
 {
-    private string[] _copList = new string[] { "S_M_Y_COP_01", "S_F_Y_COP_01", "S_M_Y_SHERIFF_01", "S_F_Y_SHERIFF_01" };
-    private string[] _copCars = new string[] { "POLICE", "POLICE2", "POLICE3", "POLICE4", "FBI", "FBI2", "SHERIFF", "SHERIFF2" };
-    private string[] _vCars = new string[] {"DUKES", "BALLER", "BALLER2", "BISON", "BISON2", "BJXL", "CAVALCADE", "CHEETAH", "COGCABRIO", "ASEA", "ADDER", "FELON", "FELON2", "ZENTORNO",
-        "WARRENER", "RAPIDGT", "INTRUDER", "FELTZER2", "FQ2", "RANCHERXL", "REBEL", "SCHWARZER", "COQUETTE", "CARBONIZZARE", "EMPEROR", "SULTAN", "EXEMPLAR", "MASSACRO",
-        "DOMINATOR", "ASTEROPE", "PRAIRIE", "NINEF", "WASHINGTON", "CHINO", "CASCO", "INFERNUS", "ZTYPE", "DILETTANTE", "VIRGO", "F620", "PRIMO", "SULTAN", "EXEMPLAR", "F620", "FELON2", "FELON", "SENTINEL", "WINDSOR",
-        "DOMINATOR", "DUKES", "GAUNTLET", "VIRGO", "ADDER", "BUFFALO", "ZENTORNO", "MASSACRO" };
-    private Ped _cop;
-    private Ped _v;
-    private Vehicle _vV;
-    private Vehicle _vCop;
-    private Vector3 _spawnPoint;
-    private Blip _blip;
-    private int _callOutMessage = 0;
-    private LHandle _pursuit;
-    private bool _pursuitCreated = false;
-    private bool _scene1 = false;
-    private bool _scene2 = false;
-    private bool _scene3 = false;
-    private bool _notificationDisplayed = false;
-    private bool _check = false;
-    private bool _hasBegunAttacking = false;
+    private static readonly string[] CopList =
+        { "S_M_Y_COP_01", "S_F_Y_COP_01", "S_M_Y_SHERIFF_01", "S_F_Y_SHERIFF_01" };
+
+    private static readonly string[] CopCars =
+        { "POLICE", "POLICE2", "POLICE3", "POLICE4", "FBI", "FBI2", "SHERIFF", "SHERIFF2" };
+
+    private static readonly string[] VCars =
+    {
+        "DUKES", "BALLER", "BALLER2", "BISON", "BISON2", "BJXL", "CAVALCADE", "CHEETAH", "COGCABRIO", "ASEA", "ADDER",
+        "FELON", "FELON2", "ZENTORNO",
+        "WARRENER", "RAPIDGT", "INTRUDER", "FELTZER2", "FQ2", "RANCHERXL", "REBEL", "SCHWARZER", "COQUETTE",
+        "CARBONIZZARE", "EMPEROR", "SULTAN", "EXEMPLAR", "MASSACRO",
+        "DOMINATOR", "ASTEROPE", "PRAIRIE", "NINEF", "WASHINGTON", "CHINO", "CASCO", "INFERNUS", "ZTYPE", "DILETTANTE",
+        "VIRGO", "F620", "PRIMO", "SULTAN", "EXEMPLAR", "F620", "FELON2", "FELON", "SENTINEL", "WINDSOR",
+        "DOMINATOR", "DUKES", "GAUNTLET", "VIRGO", "ADDER", "BUFFALO", "ZENTORNO", "MASSACRO"
+    };
+
+    private static Ped _cop;
+    private static Ped _v;
+    private static Vehicle _vV;
+    private static Vehicle _vCop;
+    private static Vector3 _spawnPoint;
+    private static Blip _blip;
+    private static int _callOutMessage;
+    private static LHandle _pursuit;
+    private static bool _pursuitCreated;
+    private static bool _scene1;
+    private static bool _scene2;
+    private static bool _scene3;
+    private static bool _notificationDisplayed;
+    private static bool _check;
+    private static bool _hasBegunAttacking;
 
     public override bool OnBeforeCalloutDisplayed()
     {
-        Random random = Rndm;
-        List<Vector3> list = new List<Vector3>();
-        Tuple<Vector3,float>[] spawningLocationList = 
+        Tuple<Vector3, float>[] spawningLocationList =
         {
-            Tuple.Create(new Vector3(-452.2763f, 5930.209f, 32.00574f),141.1158f),
-            Tuple.Create(new Vector3(2689.76f, 4379.656f, 46.21445f),123.7446f),
-            Tuple.Create(new Vector3(-2848.013f, 2205.696f, 31.40776f),117.3819f),
-            Tuple.Create(new Vector3(-1079.767f, -2050.001f, 12.78075f),223.3597f),
-            Tuple.Create(new Vector3(1901.965f, -735.1039f, 84.55292f),125.9702f),
-            Tuple.Create(new Vector3(2620.896f, 255.5361f, 97.55639f),349.3095f),
-            Tuple.Create(new Vector3(1524.368f, 820.0878f, 77.10448f),332.4926f),
-            Tuple.Create(new Vector3(2404.46f, 2872.158f, 39.88745f),307.5641f),
-            Tuple.Create(new Vector3(2913.759f, 4148.546f, 50.26934f),16.63741f),
+            Tuple.Create(new Vector3(-452.2763f, 5930.209f, 32.00574f), 141.1158f),
+            Tuple.Create(new Vector3(2689.76f, 4379.656f, 46.21445f), 123.7446f),
+            Tuple.Create(new Vector3(-2848.013f, 2205.696f, 31.40776f), 117.3819f),
+            Tuple.Create(new Vector3(-1079.767f, -2050.001f, 12.78075f), 223.3597f),
+            Tuple.Create(new Vector3(1901.965f, -735.1039f, 84.55292f), 125.9702f),
+            Tuple.Create(new Vector3(2620.896f, 255.5361f, 97.55639f), 349.3095f),
+            Tuple.Create(new Vector3(1524.368f, 820.0878f, 77.10448f), 332.4926f),
+            Tuple.Create(new Vector3(2404.46f, 2872.158f, 39.88745f), 307.5641f),
+            Tuple.Create(new Vector3(2913.759f, 4148.546f, 50.26934f), 16.63741f),
 
         };
-        for (int i = 0; i < spawningLocationList.Length; i++)
-        {
-            list.Add(spawningLocationList[i].Item1);
-        }
+        List<Vector3> list = spawningLocationList.Select(t => t.Item1).ToList();
         int num = LocationChooser.NearestLocationIndex(list);
         _spawnPoint = spawningLocationList[num].Item1;
-        _vCop = new Vehicle(_copCars[Rndm.Next(_copCars.Length)], _spawnPoint, spawningLocationList[num].Item2);
-        switch (Rndm.Next(1, 3))
+        _vCop = new Vehicle(CopCars[Rndm.Next(CopCars.Length)], _spawnPoint, spawningLocationList[num].Item2);
+        switch (Rndm.Next(1, 4))
         {
             case 1:
                 _scene1 = true;
@@ -61,14 +69,16 @@ public class TrafficStopBackupRequired : Callout
                 _scene3 = true;
                 break;
         }
-        _vV = new Vehicle(_vCars[Rndm.Next(_vCars.Length)], _vCop.GetOffsetPosition(Vector3.RelativeFront * 9f), _vCop.Heading);
+
+        _vV = new Vehicle(VCars[Rndm.Next(VCars.Length)], _vCop.GetOffsetPosition(Vector3.RelativeFront * 9f),
+            _vCop.Heading);
         _vCop.IsSirenOn = true;
         _vCop.IsSirenSilent = true;
 
         Functions.PlayScannerAudioUsingPosition("ATTENTION_ALL_UNITS OFFICER_REQUESTING_BACKUP", _spawnPoint);
         // Functions.PlayScannerAudioUsingPosition("UNITS WE_HAVE CRIME_CIVILIAN_NEEDING_ASSISTANCE_02", _SpawnPoint);
         ShowCalloutAreaBlipBeforeAccepting(_spawnPoint, 100f);
-        switch (Rndm.Next(1, 3))
+        switch (Rndm.Next(1, 4))
         {
             case 1:
                 CalloutMessage = "[UC]~w~ Traffic Stop Backup Required";
@@ -83,6 +93,7 @@ public class TrafficStopBackupRequired : Callout
                 _callOutMessage = 3;
                 break;
         }
+
         CalloutPosition = _spawnPoint;
         return base.OnBeforeCalloutDisplayed();
     }
@@ -90,25 +101,33 @@ public class TrafficStopBackupRequired : Callout
     public override bool OnCalloutAccepted()
     {
         Game.LogTrivial("UnitedCallouts Log: Traffic Stop Backup Required callout accepted.");
-        Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts", "~y~Traffic Stop Backup Required", "~b~Dispatch:~w~ A cop needs backup for a traffic stop. Respond with ~y~Code 2~w~.");
+        Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts",
+            "~y~Traffic Stop Backup Required",
+            "~b~Dispatch:~w~ A cop needs backup for a traffic stop. Respond with ~y~Code 2~w~.");
 
-        _cop = new Ped(_copList[Rndm.Next(_copList.Length)], _spawnPoint, 0f);
-        _cop.IsPersistent = true;
-        _cop.BlockPermanentEvents = true;
+        _cop = new(CopList[Rndm.Next(CopList.Length)], _spawnPoint, 0f)
+        {
+            IsPersistent = true,
+            BlockPermanentEvents = true
+        };
         _cop.Inventory.GiveNewWeapon("WEAPON_PISTOL", 500, true);
         _cop.WarpIntoVehicle(_vCop, -1);
         _cop.Tasks.CruiseWithVehicle(0, VehicleDrivingFlags.None);
         Functions.IsPedACop(_cop);
 
-        _v = new Ped(_spawnPoint);
-        _v.IsPersistent = true;
-        _v.BlockPermanentEvents = true;
+        _v = new(_spawnPoint)
+        {
+            IsPersistent = true,
+            BlockPermanentEvents = true
+        };
         _v.WarpIntoVehicle(_vV, -1);
         _v.Tasks.CruiseWithVehicle(0, VehicleDrivingFlags.None);
 
-        _blip = new Blip(_cop);
+        _blip = new(_cop)
+        {
+            Color = Color.LightBlue
+        };
         _blip.EnableRoute(Color.Blue);
-        _blip.Color = Color.LightBlue;
         return base.OnCalloutAccepted();
     }
 
@@ -122,55 +141,58 @@ public class TrafficStopBackupRequired : Callout
 
     public override void Process()
     {
-        GameFiber.StartNew(delegate
+        if (_spawnPoint.DistanceTo(MainPlayer) < 25f)
         {
-            if (_spawnPoint.DistanceTo(MainPlayer) < 25f)
+            if (_scene1 && !_scene3 && !_scene2 && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot &&
+                !_hasBegunAttacking)
             {
-                if (_scene1 == true && _scene3 == false && _scene2 == false && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot && !_hasBegunAttacking)
-                {
-                    _v.Tasks.LeaveVehicle(_vV, LeaveVehicleFlags.None);
-                    _v.Health = 200;
-                    _cop.Tasks.LeaveVehicle(_vCop, LeaveVehicleFlags.LeaveDoorOpen);
-                    GameFiber.Wait(200);
-                    new RelationshipGroup("Cop");
-                    new RelationshipGroup("V");
-                    _v.RelationshipGroup = "V";
-                    _cop.RelationshipGroup = "Cop";
-                    Game.SetRelationshipBetweenRelationshipGroups("Cop", "V", Relationship.Hate);
-                    _v.Inventory.GiveNewWeapon("WEAPON_PISTOL", 500, true);
-                    _v.Tasks.FightAgainstClosestHatedTarget(1000f);
-                    _cop.Tasks.FightAgainstClosestHatedTarget(1000f);
-                    GameFiber.Wait(2000);
-                    _v.Tasks.FightAgainst(MainPlayer);
-                    _hasBegunAttacking = true;
-                    GameFiber.Wait(600);
-                }
-                if (_scene2 == true && _scene3 == false && _scene1 == false && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot && !_notificationDisplayed && !_check)
-                {
-                    _cop.Tasks.LeaveVehicle(_vCop, LeaveVehicleFlags.LeaveDoorOpen);
-                    GameFiber.Wait(600);
-                    NativeFunction.CallByName<uint>("TASK_AIM_GUN_AT_ENTITY", _cop, _v, -1, true);
-                    Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts", "", "Perform a traffic stop.");
-                    _notificationDisplayed = true;
-                    Game.DisplayHelp("Press the ~y~END~w~ key to end the Traffic Stop Backup callout.", 5000);
-                    GameFiber.Wait(600);
-                    Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts", "~y~Dispatch", "Loading ~g~Informations~w~ of the ~y~LSPD Database~w~...");
-                    Functions.DisplayVehicleRecord(_vV, true);
-                    _check = true;
-                }
-                if (_scene3 == true && _scene1 == false && _scene2 == false && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot)
-                {
-                    _pursuit = Functions.CreatePursuit();
-                    Functions.AddPedToPursuit(_pursuit, _v);
-                    Functions.SetPursuitIsActiveForPlayer(_pursuit, true);
-                    _pursuitCreated = true;
-                }
+                _v.Tasks.LeaveVehicle(_vV, LeaveVehicleFlags.None);
+                _v.Health = 200;
+                _cop.Tasks.LeaveVehicle(_vCop, LeaveVehicleFlags.LeaveDoorOpen);
+                GameFiber.Wait(200);
+                var vRelationshipGroup = new RelationshipGroup("V");
+                _v.RelationshipGroup = vRelationshipGroup;
+                _cop.RelationshipGroup = RelationshipGroup.Cop;
+                Game.SetRelationshipBetweenRelationshipGroups(RelationshipGroup.Cop, vRelationshipGroup, Relationship.Hate);
+                Game.SetRelationshipBetweenRelationshipGroups(MainPlayer.RelationshipGroup, vRelationshipGroup, Relationship.Hate);
+                _v.Inventory.GiveNewWeapon("WEAPON_PISTOL", 500, true);
+                _v.Tasks.FightAgainstClosestHatedTarget(1000f);
+                _cop.Tasks.FightAgainstClosestHatedTarget(1000f);
+                GameFiber.Wait(2000);
+                _hasBegunAttacking = true;
+                GameFiber.Wait(600);
             }
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
-            if (_v && _v.IsDead) End();
-            if (_v && Functions.IsPedArrested(_v)) End();
-        }, "Traffic Stop Backup Required [UnitedCallouts]");
+
+            if (_scene2 && !_scene3 && !_scene1 && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot &&
+                !_notificationDisplayed && !_check)
+            {
+                _cop.Tasks.LeaveVehicle(_vCop, LeaveVehicleFlags.LeaveDoorOpen);
+                GameFiber.Wait(600);
+                NativeFunction.CallByName<uint>("TASK_AIM_GUN_AT_ENTITY", _cop, _v, -1, true);
+                Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts", "",
+                    "Perform a traffic stop.");
+                _notificationDisplayed = true;
+                Game.DisplayHelp("Press the ~y~END~w~ key to end the Traffic Stop Backup callout.", 5000);
+                GameFiber.Wait(600);
+                Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts",
+                    "~y~Dispatch", "Loading ~g~Informations~w~ of the ~y~LSPD Database~w~...");
+                Functions.DisplayVehicleRecord(_vV, true);
+                _check = true;
+            }
+
+            if (_scene3 && !_scene1 && !_scene2 && _cop.DistanceTo(MainPlayer) < 25f && MainPlayer.IsOnFoot)
+            {
+                _pursuit = Functions.CreatePursuit();
+                Functions.AddPedToPursuit(_pursuit, _v);
+                Functions.SetPursuitIsActiveForPlayer(_pursuit, true);
+                _pursuitCreated = true;
+            }
+        }
+
+        if (MainPlayer.IsDead) End();
+        if (Game.IsKeyDown(Settings.EndCall)) End();
+        if (_v && _v.IsDead) End();
+        if (_v && Functions.IsPedArrested(_v)) End();
         base.Process();
     }
 
@@ -181,7 +203,8 @@ public class TrafficStopBackupRequired : Callout
         if (_vV) _vV.Dismiss();
         if (_vCop) _vCop.Dismiss();
         if (_blip) _blip.Delete();
-        Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts", "~y~Traffic Stop Backup Required", "~b~You: ~w~Dispatch we're code 4. Show me ~g~10-8.");
+        Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~UnitedCallouts",
+            "~y~Traffic Stop Backup Required", "~b~You: ~w~Dispatch we're code 4. Show me ~g~10-8.");
         Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
         base.End();
     }
